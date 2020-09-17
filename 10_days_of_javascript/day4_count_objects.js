7dc0c235da22c68d6eb31592723ba755d79164a4
hall500 <hallhomoms22@gmail.com>
2020-09-18 00:47:50 +0100

    Day 4 of 10 days of javascript

---
+++ 10_days_of_javascript/day4_count_objects.js
@@ -0,0 +1,38 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    let count = 0;
+    objects.forEach((o) => {
+        if(o.x === o.y) count++;
+    });
+    return count;
+}
+
