07721653dba2413ac1c7c63b66584880c4eb1e29
hall500 <momohabdullah20@gmail.com>
2021-01-10 00:35:11 +0100

    Update Abbreviation.py

--- Interview_Prep_Kit/Dynamic_Programming/Abbreviation.py
+++ Interview_Prep_Kit/Dynamic_Programming/Abbreviation.py
@@ -8,21 +8,21 @@ import sys
 
 # Complete the abbreviation function below.
 def abbreviation(a, b):
-    if len(a) < len(b):
-        return("NO")
-    j=len(b)-1
-    res=[]
-    n=-1*len(a)
-    for i in range(-1,n-1,-1):
-        if(a[i].upper()==b[j] and j>(len(b)*-1)):
-            if a[i].islower():
-                res.append(a[i])
-            j-=1
-        elif a[i].isupper() and a[i].lower() in res:
-            res.remove(a[i].lower())
-        elif a[i].isupper():
-            return("NO")
-    return("YES")
+    m, n = len(a), len(b)
+    dp = [[False]*(m+1) for _ in range(n+1)]
+    dp[0][0] = True
+    for i in range(n+1):
+        for j in range(m+1):
+            if i == 0 and j != 0:
+                dp[i][j] = a[j-1].islower() and dp[i][j-1]
+            elif i != 0 and j != 0:
+                if a[j-1] == b[i-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                elif a[j-1].upper() == b[i-1]:
+                    dp[i][j] = dp[i-1][j-1] or dp[i][j-1]
+                elif not (a[j-1].isupper() and b[i-1].isupper()):
+                    dp[i][j] = dp[i][j-1]
+    return "YES" if dp[n][m] else "NO"
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
